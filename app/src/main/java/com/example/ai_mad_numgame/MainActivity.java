2884a996d52da9ef8ed61ad25bc2450424420421
imani03 <201b045@juetguna.in>
2022-05-21 13:09:40 +0530

    new interface

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -30,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
     int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
     int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
     String operators[]={"+","-","*","/"};
-    int correctButton=0; //which button will have the correct answer (tag of that button)
-    Random random=new Random(); //You will generate randdom alegebra questions
+    Random random=new Random();
+    int correctButton= random.nextInt(3); //which button will have the correct answer (tag of that button)
+     //You will generate random algebra questions
     TextView textView2;
     Button button1,button2,button3,button4;
     public void load(View view){
@@ -72,14 +73,37 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2= random.nextInt(10);
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
+        int correctAnswer=-100;
+        if(operator.equals("+")){
+            correctAnswer=operand1+operand2;
+        }
+        else if(operator.equals("*")) {
+            correctAnswer = operand1 * operand2;
+        }
+        else if (operator.equals("/"))
+        {
+            correctAnswer=operand1/operand2;
+        }
+        else(operator.equals("-"))
+        {
+            correctAnswer=operand1-operand2;
+        }
+      // Your code here, to display correct and incorrect options on the buttons
+        if(correctButton==0)
+        {
+            button1.setText(Integer.toString());
+            button2.setText(operand1*operand2 + " ");
+            button3.setText(operand1/operand2 + " ");
+            button4.setText(operand1-operand2 + " ");
 
-      // Your code here, to diplay correct and incorrect options on the buttons
+        }
 
-        if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
+
+        if(matchCounter==3){    // if three matches are completed update the performance in sharedPreferences
 
             matchCounter=0;
 
